1a2e52197dc5915897306a71541b368c44390c0a
henhen02 <hendri21072002@gmail.com>
2023-11-15 09:06:47 +0700

    Nothing

--- lib/presentation/pages/pendaftaran/pasien_baru/form_step/pendaftaran_pasien_baru_step_4.dart
+++ lib/presentation/pages/pendaftaran/pasien_baru/form_step/pendaftaran_pasien_baru_step_4.dart
@@ -1,38 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:labkesda_mobile/presentation/components/buttons/step_buttton.dart';
 import 'package:labkesda_mobile/presentation/styles/styles.dart';
 import 'package:labkesda_mobile/models/value_dropdown/value_dropdown.dart';
 import 'package:labkesda_mobile/presentation/components/input/dropdown_input.dart';
-import 'package:labkesda_mobile/presentation/components/buttons/direct_button.dart';
 import 'package:labkesda_mobile/presentation/components/layouts/title_form_layout.dart';
 import 'package:labkesda_mobile/presentation/components/input/text_form_field_input.dart';
 import 'package:labkesda_mobile/presentation/pages/pendaftaran/pasien_baru/pendaftaran_pasien_baru_page.dart';
 
-final List<ValueDropdown> agama = [
+final List<ValueDropdown> golonganDarah = [
   ValueDropdown(
-    teks: "Islam",
-    value: "islam",
+    teks: "A",
+    value: "a",
   ),
   ValueDropdown(
-    teks: "Kristen",
-    value: "kristen",
+    teks: "B",
+    value: "b",
   ),
   ValueDropdown(
-    teks: "Katolik",
-    value: "katolik",
+    teks: "AB",
+    value: "ab",
   ),
   ValueDropdown(
-    teks: "Hindu",
-    value: "hindu",
+    teks: "O",
+    value: "o",
   ),
   ValueDropdown(
-    teks: "Budha",
-    value: "budha",
-  ),
-  ValueDropdown(
-    teks: "Konghucu",
-    value: "konghucu",
+    teks: "Tidak Tahu",
+    value: "tidak_tahu",
   ),
 ];
 
@@ -43,7 +39,7 @@ class PendaftaranPasienBaruStep4 extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final selectedAgama = useState(null);
+    final selectedGolonganDarah = useState(null);
 
     return Container(
       padding: const EdgeInsets.all(20),
@@ -62,88 +58,82 @@ class PendaftaranPasienBaruStep4 extends HookConsumerWidget {
             height: 20,
           ),
           Text(
-            'Nama',
+            'Alamat Domisili',
             style: AppStyle.inputLabel,
           ),
           const SizedBox(
             height: 5,
           ),
           const TextFormFieldInput(
-            placeHolder: 'Masukan nama lengkap',
+            placeHolder: 'Masukkan alamat domisili',
           ),
           const SizedBox(
             height: 20,
           ),
           Text(
-            'Templat Lahir',
+            'Golongan Darah',
             style: AppStyle.inputLabel,
           ),
           const SizedBox(
             height: 5,
           ),
-          const TextFormFieldInput(
-            placeHolder: 'Masukan tempat lahir',
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Text(
-            'Tanggal Lahir',
-            style: AppStyle.inputLabel,
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          TextFormFieldInput(
-            readOnly: true,
-            placeHolder: 'Masukan tanggal lahir',
-            suffixIcon: const Icon(Icons.date_range),
-            onTap: () {
-              showDatePicker(
-                context: context,
-                initialDate: DateTime.now(),
-                firstDate: DateTime(1999),
-                lastDate: DateTime(2030),
-              );
-            },
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Text(
-            'NIK',
-            style: AppStyle.inputLabel,
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          const TextFormFieldInput(
-            placeHolder: 'Masukan NIK',
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Text(
-            'Agama',
-            style: AppStyle.inputLabel,
-          ),
-          const SizedBox(
-            height: 5,
-          ),
+          // Bikin radio button
           DropdownInput(
-            values: agama,
-            selectedValue: selectedAgama,
-            placeHolder: "Pilih Agama",
+            values: golonganDarah,
+            selectedValue: selectedGolonganDarah,
+            placeHolder: "Pilih Golongan Darah",
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Nomor Telepon/Whatsapp',
+            style: AppStyle.inputLabel,
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          const TextFormFieldInput(
+            keyboardType: TextInputType.phone,
+            placeHolder: 'Masukkan nomor telepon/whatsapp',
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Email',
+            style: AppStyle.inputLabel,
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          const TextFormFieldInput(
+            keyboardType: TextInputType.emailAddress,
+            placeHolder: 'Masukkan email',
           ),
           const SizedBox(
             height: 40,
           ),
-          DirectButton(
-            text: 'SELANJUTNYA',
-            onPressed: () {
-              currIndexStepper.value++;
-              stepScrollController.jumpTo(0);
-            },
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              StepperButton(
+                text: "Kembali",
+                buttonType: "prev",
+                onPressed: () {
+                  currIndexStepper.value--;
+                  stepScrollController.jumpTo(0);
+                },
+              ),
+              StepperButton(
+                text: "Lanjutkan",
+                buttonType: "next",
+                onPressed: () {
+                  currIndexStepper.value++;
+                  stepScrollController.jumpTo(0);
+                },
+              ),
+            ],
           ),
           const SizedBox(
             height: 40,
